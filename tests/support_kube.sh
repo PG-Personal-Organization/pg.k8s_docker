faeb559fc3ebc8817384a6f2c45a1147b14a13de
Patryk Grzywacz <patrykg198@gmail.com>
2025-09-10 20:50:36 +0200

    node exporter fix script

--- tests/support_kube.sh
+++ tests/support_kube.sh
@@ -5,3 +5,15 @@ kubectl create namespace ingress-nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 
 ----
+
+---- Node exporter ---
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install monitoring prometheus-community/kube-prometheus-stack -n monitoring --create-namespace
+
+sleep 3
+
+kubectl delete daemonset monitoring-prometheus-node-exporter -n monitoring
+kubectl delete service monitoring-prometheus-node-exporter -n monitoring
+
+----
